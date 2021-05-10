ef3f17a8fcf26be9e1af5a6bf4eec92111120953
constantoh <ohconstant@naver.com>
2021-05-10 23:22:15 +0900

    gitignore commit

---
+++ src/main/java/com/sangsoo/book/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,21 @@
+package com.sangsoo.book.springboot.web.dto;
+
+import com.sangsoo.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
