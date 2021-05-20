0c2220db1a64e0a80884955d900e114c6cf83e7f
constantoh <ohconstant@naver.com>
2021-05-20 16:01:53 +0900

    application-real

--- src/main/java/com/sangsoo/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/sangsoo/book/springboot/config/auth/SecurityConfig.java
@@ -6,6 +6,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+/**
+ * Spring Security config
+ *
+ * @EnableWebSecurity -> Spring Security 설정 활성화
+ * */
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
